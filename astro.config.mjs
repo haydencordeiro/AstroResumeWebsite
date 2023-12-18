ad7f20fc6fe02cf1438c143bc813c4b7137d6742
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-12-17 21:51:58 -0500

    updated domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://newresume.hayden.co.in/',
+    site: 'https://hayden.co.in/',
 });
