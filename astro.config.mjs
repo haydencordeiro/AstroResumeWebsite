baf0e0fadafe7f4902d7ae7cda19fd2d56f9eaff
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-06-22 12:22:59 -0400

    Changes for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hayden.co.in/',
+  site: 'https://hayden.co.in/',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date('2024-06-22'),
+    }),
+  ]
 });
